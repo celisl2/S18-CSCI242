b0ee180116fa8553abcad192660fde5c965711da
Andrew Besmer <besmera@winthrop.edu>
2018-02-13 13:47:18 -0500

    Class example

--- exceptions/index.php
+++ exceptions/index.php
@@ -10,19 +10,66 @@ require_once('RatedRException.php');
 $colton = new User();
 $colton->setUsername("Colton");
 $colton->setAge(2);
+$colton->setSubscription(true);
 
 $home = new Movie();
 $home->setRating("G");
 $home->setTitle("Home[2016]");
 
+/*
 try {
-    Mp4Stream::getMp4Stream($home, $colton);
+    echo Mp4Stream::getMp4Stream($home, $colton);
 }
 catch(NoSubscriptionException $ex)
 {
     echo "Please purchase a subscription";
 }
+ * 
+ */
 
 
+$it = new Movie();
+$it->getTitle("It[2017]");
+$it->setRating('R');
 
+/*
+try {
+    echo Mp4Stream::getMp4Stream($it, $colton);
+}
+catch(NoSubscriptionException $ex)
+{
+    echo "Please purchase a subscription";
+}
+catch(RatedRException $ex)
+{
+    echo "Sorry, you aren't able to watch movies rated R.";
+}
 
+try {
+    echo Mp4Stream::getMp4Stream($it, $colton);
+}
+catch(Exception $ex)
+{
+    //mail the admin and let them know some error has occured
+    //show a picture of a unicorn and say sorry
+}
+
+*/
+
+try {
+    echo Mp4Stream::getMp4Stream($it, $colton);
+    //another line of code
+}
+catch(NoSubscriptionException $ex)
+{
+    echo "Please purchase a subscription";
+}
+catch(RatedRException $ex)
+{
+    echo "Sorry, you aren't able to watch movies rated R.";
+}
+catch(Exception $ex)
+{
+    //mail the admin and let them know some error has occured
+    //show a picture of a unicorn and say sorry
+}
